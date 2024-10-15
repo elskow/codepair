415b9b337e43d86df4e0579c45cdf65f6de97273
elskow <elskow@outlook.com>
2024-10-15 20:03:05 +0700

    feat: implement the videochatws handler (gateway)

--- gateway/go.mod
+++ gateway/go.mod
@@ -3,7 +3,9 @@ module github.com/elskow/codepair/gateway
 go 1.22.7
 
 require (
+	github.com/fasthttp/websocket v1.5.3
 	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/gofiber/websocket/v2 v2.2.1
 	github.com/valyala/fasthttp v1.51.0
 )
 
@@ -15,6 +17,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- gateway/server.go
+++ gateway/server.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/fasthttp/websocket"
 	"github.com/gofiber/fiber/v2"
+	fiberWebsocket "github.com/gofiber/websocket/v2"
 )
 
 type Server struct {
@@ -45,8 +51,98 @@ func (s *Server) setupRoutes() {
 }
 
 func (s *Server) handleVideochatWebSocket(c *fiber.Ctx) error {
-	// TODO: Implement proper WebSocket handling for videochat
-	return proxyRequest(videochatURL)(c)
+	return fiberWebsocket.New(func(conn *fiberWebsocket.Conn) {
+		defer conn.Close()
+
+		roomID := c.Params("roomID")
+		if roomID == "" {
+			log.Println("Room ID is required")
+			return
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		videochatURL := fmt.Sprintf("ws://%s/ws/%s", videochatURL, roomID)
+		videochatConn, err := s.connectToVideochatService(videochatURL)
+		if err != nil {
+			log.Printf("Failed to connect to videochat service: %v", err)
+			return
+		}
+		defer videochatConn.Close()
+
+		go s.relayMessages(ctx, conn, videochatConn)
+		go s.relayMessages(ctx, videochatConn, conn)
+
+		<-ctx.Done()
+	})(c)
+}
+
+func (s *Server) connectToVideochatService(url string) (*websocket.Conn, error) {
+	dialer := websocket.Dialer{
+		Proxy:            http.ProxyFromEnvironment,
+		HandshakeTimeout: 45 * time.Second,
+	}
+
+	headers := http.Header{}
+	headers.Add("Origin", "http://localhost:8000")
+
+	conn, _, err := dialer.Dial(url, headers)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to videochat service: %v", err)
+	}
+
+	return conn, nil
+}
+
+func (s *Server) relayMessages(ctx context.Context, src interface{}, dst interface{}) {
+	var srcConn, dstConn interface {
+		ReadMessage() (messageType int, p []byte, err error)
+		WriteMessage(messageType int, data []byte) error
+	}
+
+	switch v := src.(type) {
+	case *fiberWebsocket.Conn:
+		srcConn = v
+	case *websocket.Conn:
+		srcConn = v
+	default:
+		log.Printf("Unsupported source connection type")
+		return
+	}
+
+	switch v := dst.(type) {
+	case *fiberWebsocket.Conn:
+		dstConn = v
+	case *websocket.Conn:
+		dstConn = v
+	default:
+		log.Printf("Unsupported destination connection type")
+		return
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			messageType, message, err := srcConn.ReadMessage()
+			if err != nil {
+				if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+					log.Println("WebSocket closed normally")
+				} else {
+					log.Printf("Error reading WebSocket message: %v", err)
+				}
+				return
+			}
+
+			err = dstConn.WriteMessage(messageType, message)
+			if err != nil {
+				log.Printf("Error writing WebSocket message: %v", err)
+				return
+			}
+		}
+	}
 }
 
 func (s *Server) handleEditorWebSocket(c *fiber.Ctx) error {
