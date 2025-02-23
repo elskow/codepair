51578a9d2135a2410a59abf993846921a3ad04e8
elskow <xlirby@gmail.com>
2025-02-23 18:22:25 +0700

    refactor: move the handler related to the specified file

--- peer-cp/server/editor.go
+++ peer-cp/server/editor.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 
 	"github.com/gofiber/websocket/v2"
-	"github.com/pion/webrtc/v4"
 	"go.uber.org/zap"
 )
 
@@ -20,89 +19,6 @@ type EditorMessage struct {
 type Cursor struct {
 	Line   int `json:"line"`
 	Column int `json:"column"`
-}
-
-func (s *Server) HandleEditorWS(c *websocket.Conn) {
-	defer c.Close()
-
-	ctx := context.WithValue(context.Background(), "requestID", c.Params("requestId"))
-	logger := s.getLogger(ctx)
-
-	roomID := c.Params("roomId")
-	token := c.Query("token")
-
-	validRoom, err := s.validateRoom(roomID, token)
-	if err != nil {
-		logger.Error("Room validation failed", zap.Error(err))
-		return
-	}
-
-	if !validRoom.IsActive {
-		logger.Error("Room is not active")
-		return
-	}
-
-	client := &EditorClient{
-		conn: c,
-	}
-
-	s.roomsMutex.Lock()
-	localRoom, exists := s.rooms[roomID]
-	if !exists {
-		localRoom = &Room{
-			editorClients: make(map[*websocket.Conn]*EditorClient),
-			webrtcClients: make(map[*websocket.Conn]*WebRTCClient),
-			peerConns:     make(map[string]*webrtc.PeerConnection),
-		}
-		s.rooms[roomID] = localRoom
-	}
-	s.roomsMutex.Unlock()
-
-	localRoom.clientsMutex.Lock()
-	localRoom.editorClients[c] = client
-	localRoom.clientsMutex.Unlock()
-
-	logger.Info("Editor client connected", zap.String("roomID", roomID))
-
-	// Send current code state to new client
-	if localRoom.currentCode != "" {
-		syncMessage := EditorMessage{
-			Type:     "sync",
-			Code:     localRoom.currentCode,
-			Language: localRoom.language,
-		}
-		if err := c.WriteJSON(syncMessage); err != nil {
-			logger.Error("Failed to send sync message", zap.Error(err))
-		}
-	}
-
-	// Handle incoming messages
-	for {
-		var msg EditorMessage
-		err := c.ReadJSON(&msg)
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				logger.Error("WebSocket error", zap.Error(err))
-			}
-			break
-		}
-
-		s.handleEditorMessage(ctx, c, roomID, msg)
-	}
-
-	// Cleanup when client disconnects
-	localRoom.clientsMutex.Lock()
-	delete(localRoom.editorClients, c)
-	localRoom.clientsMutex.Unlock()
-
-	s.roomsMutex.Lock()
-	if len(localRoom.editorClients) == 0 && len(localRoom.webrtcClients) == 0 {
-		delete(s.rooms, roomID)
-		logger.Info("Room closed", zap.String("roomID", roomID))
-	}
-	s.roomsMutex.Unlock()
-
-	logger.Info("Editor client disconnected", zap.String("roomID", roomID))
 }
 
 func (s *Server) handleEditorMessage(ctx context.Context, c *websocket.Conn, roomID string, msg EditorMessage) {
